352c10f542fb98db0279286eeea347bb41ee5dfb
妹妹 <63705532+arthurtibame@users.noreply.github.com>
2020-07-07 23:58:27 +0800

    Update __init__.py

--- stock_mvc/Stockapp/app/__init__.py
+++ stock_mvc/Stockapp/app/__init__.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 import  os
 app.config.from_object('app.utils.setting')     #模块下的setting文件名，不用加py后缀 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)   #环境变量，指向配置文件setting的路径
-app.config['SQLALCHEMY_DATABASE_URL'] = r"mysql+pymysql://root:!@#sam@5678/*-@3.1.75.82:3306/StockEveryday"
+app.config['SQLALCHEMY_DATABASE_URL'] = #mysql://username:password@server/db
 
-#mysql://username:password@server/db
 
 
         
